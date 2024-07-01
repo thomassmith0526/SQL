d75bac34918004acc057133586df5a32dd24480e
thomassmith0526 <thomassmith0526@gmail.com>
2024-07-01 16:59:24 -0500

    getting there

--- JS/index.js
+++ JS/index.js
@@ -1,68 +1,144 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const express = require('express');
-// Import and require Pool (node-postgres)
-// We'll be creating a Connection Pool. Read up on the benefits here: https://node-postgres.com/features/pooling
-const { Pool } = require('pg');
+const express = require("express");
+const { Pool } = require("pg");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-const listQuestions =  {
+const listQuestions = [
+  {
     type: "list",
     message: "What would you like to do?",
     name: "actions",
     choices: [
-      "Add Employee",
-      "Update Employee Role",
-      "View all Roles",
-      "Add Role",
+      "View All Employees",
+      "View All Roles",
       "View All Departments",
+      "Add Employees",
+      "Update Employee Role",
+      "Add Role",
       "Add Departments",
       "Quit",
     ],
-  }
+  },
+];
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 // Connect to database
 const pool = new Pool(
   {
-    
-    user: 'postgres',
-    password: 'texas',
-    host: 'localhost',
-    database: 'thomassports_db'
-},
-console.log('Connected to the thomasspots_db database!')
-)
+    user: "postgres",
+    password: "texas",
+    host: "localhost",
+    database: "thomassports_db",
+  },
+  console.log("Connected to the thomasspots database!")
+);
 
 pool.connect();
 
-
 function init() {
-    inquirer
-  .prompt(listQuestions)
-    .then((answers) => {
-    console.log(answers.actions)
- })
+  inquirer.prompt(listQuestions).then((answers) => {
+    console.log(answers.actions);
+    switch (answers.actions) {
+      case "View All Employees":
+        let tsVAE = "SELECT * FROM employees";
+        pool.query(tsVAE, function (err, { rows }) {
+          console.log(rows);
+        });
+        break;
 
- switch (listQuestions) {
-    case: 'Add Employees' 
-    //
-    break;
+      case "View All Roles":
+        let tsVAR = "SELECT * FROM roles";
+        pool.query(tsVAR, function (err, { rows }) {
+          console.log(rows);
+        });
+        break;
 
+      case "View All Departments":
+        let tsVAD = "SELECT * FROM departments";
+        pool.query(tsVAD, function (err, { rows }) {
+          console.log(rows);
+        });
+        break;
 
- }
+      case "Add Employees":
+        inquirer
+    .prompt([
+         {
+                type: "input",
+                message: "What is the new employees first name",
+                name: "first",               
+         }, 
+         {
+            type:"input",
+            message: "What is the new employees last name?",
+            name: "last",
+         },
+        {
+            type: "input",
+            message: "What is the new employees role ID?",
+            name: "role_id",
+        },
+        {
+            type: "input",
+            message: "What is the employees manager ID?",
+            name: "man_id",
+        },
+        
+    ])
+    .then((answers) => {   
+        let ts = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('${answers.first}',)'${answers.last}', '${answers.role_id}', '${answers.man_id}'`;
+        if (answers.man_id === '') {
+          answers.man_id = null;
+        }
+        pool.query(ts, function (err, result) {
+          if (err) {
+            console.error('Error:', err);
+          } else {
+            console.log('new employee added')
+          }
+        });
+     
+    });
+   
+        break;
 
+      case "Update Employee Role":
+        const newRoleID = "";
+        let tsUER = `UPDATE employee SET role = '${newRoleID}'`;
+        pool.query(tsUER, function (err, result) {
+          console.log(result +  "records updated");
+        });
+        break;
 
+      case "Add Role":
+        let title = "";
+        let salary = "";
+        let department = "";
+        let tsAR = `INSERT INTO roles = '${title}, ${salary}, ${department}'`;
+        pool.query(tsAR, function (err, result) {
+          console.log(result + "role added");
+        });
+        break;
+
+      case "Add Department":
+        let newDep = "";
+        let tsAD = `INSERT INTO departments = '${newDep}'`;
+        pool.query(tsAD, function (err, result) {
+          console.log(result + "department added");
+        });
+        break;
+      default:
+        process.exit();
+    }
+  });
 }
 
-init()
-
+init();
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-  
+  console.log(`Server running on port ${PORT}`);
+});
